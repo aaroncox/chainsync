1614c9c2d80b4150b30aaa1f775636a4cf67b9da
Aaron Cox <aaron.cox@greymass.com>
2018-03-08 23:47:58 -0500

    example steem calls

---
+++ example_steem.py
@@ -0,0 +1,17 @@
+import datetime
+from blocksync import Blocksync
+
+s = Blocksync(endpoints=['https://api.steemitstage.com'])
+
+print('Getting block 1')
+block = s.get_block(1)
+print(block)
+
+print('Getting blocks 1-5')
+blocks = s.get_blocks(1, 5)
+for block in blocks:
+    print(block)
+
+print('Streaming blocks...')
+for block in s.get_block_stream(batch_size=100, mode='irreversible'):
+    print("{}: {} - {}".format(datetime.datetime.now(), block['height'], block['witness']))
