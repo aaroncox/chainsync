82aeb20e36f5e8f6be7b8055675d89e321642c2e
Aaron Cox <aaron.cox@greymass.com>
2018-03-16 00:08:41 -0400

    changed example to use 0.19.2 api + spacing

--- example_steem.py
+++ example_steem.py
@@ -1,25 +1,25 @@
 import datetime
 from blocksync import Blocksync
 
-s = Blocksync(endpoints=['https://api.steemitstage.com'])
+s = Blocksync(endpoints=['https://api.steemit.com'])
 
-print('Getting block 1')
+print('\nGetting block 1')
 block = s.get_block(1)
 print(block)
 
-print('Getting blocks 1-5')
+print('\nGetting blocks 1-5')
 blocks = s.get_blocks(1, 5)
 for block in blocks:
     print(block)
 
-print('Streaming blocks...')
+print('\nStreaming blocks...')
 for block in s.get_block_stream(batch_size=100, mode='irreversible'):
     print("{}: {} - {}".format(datetime.datetime.now(), block['block_num'], block['witness']))
 
-print('Streaming all ops...')
+print('\nStreaming all ops...')
 for op in s.get_op_stream():
     print("{}: {} - {}".format(datetime.datetime.now(), op['block_num'], op['operation_type']))
 
-print('Streaming vote ops only...')
+print('\nStreaming vote ops only...')
 for op in s.get_op_stream(whitelist=['vote']):
     print("{}: {} - {} by {}".format(datetime.datetime.now(), op['block_num'], op['operation_type'], op['voter']))
