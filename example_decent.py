d80547c4f427c2f9ac908c0d368c59ec6b46e7f3
Aaron Cox <aaron.cox@greymass.com>
2018-03-16 00:24:06 -0400

    updated examples

--- example_decent.py
+++ example_decent.py
@@ -1,28 +1,27 @@
 import datetime
 from blocksync import Blocksync
 from blocksync.adapters.decent import DecentAdapter
-from blocksync.adapters.steem import SteemAdapter
 
-a = DecentAdapter(endpoints=['http://api.decent-db.com:8090/'])
-s = Blocksync(adapter=a, debug=True)
+adapter = DecentAdapter(endpoints=['http://api.decent-db.com:8090/'])
+blocksync = Blocksync(adapter)
 
 print('Getting block 1')
-block = s.get_block(1)
+block = blocksync.get_block(1)
 print(block)
 
 print('Getting blocks 1-5')
-blocks = s.get_blocks(1, 5)
+blocks = blocksync.get_blocks(1, 5)
 for block in blocks:
     print(block)
 
 print('Streaming blocks...')
-for block in s.get_block_stream(batch_size=100, mode='irreversible'):
+for block in blocksync.get_block_stream(batch_size=100, mode='irreversible'):
     print("{}: {} - {}".format(datetime.datetime.now(), block['block_num'], block['miner']))
 
 print('Streaming all ops...')
-for op in s.get_op_stream():
+for op in blocksync.get_op_stream():
     print("{}: {} - {}".format(datetime.datetime.now(), op['block_num'], op['operation_type']))
 
 print('Streaming vote ops only...')
-for op in s.get_op_stream(whitelist=['vote']):
+for op in blocksync.get_op_stream(whitelist=['vote']):
     print("{}: {} - {} by {}".format(datetime.datetime.now(), op['block_num'], op['operation_type'], op['voter']))
--- example_steem.py
+++ example_steem.py
@@ -1,25 +1,27 @@
 import datetime
 from blocksync import Blocksync
+from blocksync.adapters.steem import SteemAdapter
 
-s = Blocksync(endpoints=['https://api.steemit.com'])
+adapter = SteemAdapter(endpoints=['https://api.steemit.com'])
+blocksync = Blocksync(adapter)
 
 print('\nGetting block 1')
-block = s.get_block(1)
+block = blocksync.get_block(1)
 print(block)
 
 print('\nGetting blocks 1-5')
-blocks = s.get_blocks(1, 5)
+blocks = blocksync.get_blocks(1, 5)
 for block in blocks:
     print(block)
 
 print('\nStreaming blocks...')
-for block in s.get_block_stream(batch_size=100, mode='irreversible'):
+for block in blocksync.get_block_stream(batch_size=100, mode='irreversible'):
     print("{}: {} - {}".format(datetime.datetime.now(), block['block_num'], block['witness']))
 
 print('\nStreaming all ops...')
-for op in s.get_op_stream():
+for op in blocksync.get_op_stream():
     print("{}: {} - {}".format(datetime.datetime.now(), op['block_num'], op['operation_type']))
 
 print('\nStreaming vote ops only...')
-for op in s.get_op_stream(whitelist=['vote']):
+for op in blocksync.get_op_stream(whitelist=['vote']):
     print("{}: {} - {} by {}".format(datetime.datetime.now(), op['block_num'], op['operation_type'], op['voter']))
