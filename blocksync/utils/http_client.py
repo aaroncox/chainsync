48d49fadf47b2e92c51c4d91c124faa97fd6eb7d
Aaron Cox <aaron.cox@greymass.com>
2018-03-16 02:12:46 -0400

    better handling of errors

--- blocksync/utils/http_client.py
+++ blocksync/utils/http_client.py
@@ -1,24 +1,27 @@
 import requests
-from jsonrpcclient.http_client import HTTPClient
-from jsonrpcclient.request import Request
-from jsonrpcclient.prepared_request import PreparedRequest
 from jsonrpcclient import config
+from jsonrpcclient.exceptions import ParseResponseError
+from jsonrpcclient.http_client import HTTPClient
+from jsonrpcclient.prepared_request import PreparedRequest
+from jsonrpcclient.request import Request
 config.validate = False
 
 class HttpClient(HTTPClient):
 
     def send(self, request, **kwargs):
         response = super(HttpClient, self).send(request, **kwargs)
-        if 'error' in response:
+        if not response or 'error' in response:
             print("response")
             print(response)
             raise requests.RequestException()
         return response
 
     def request(self, method_name, *args, **kwargs):
-        response = super(HttpClient, self).send(Request(method_name, *args, **kwargs))
-        if 'error' in response:
-            print("response")
-            print(response)
-            raise requests.RequestException()
-        return response
+        try:
+            response = super(HttpClient, self).send(Request(method_name, *args, **kwargs))
+            if not response or 'error' in response:
+                print("response")
+                print(response)
+                raise requests.RequestException()
+            return response
+        except ParseResponseError:
