e5da5394a93cdf954542d681423c1e3cc1ac904d
caleb logan <clogan202@gmail.com>
2018-03-17 13:24:36 -0700

    Add requests to requirements and dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name='blocksync',
-      version='0.1.0',
-      description='Utility to stream and sync blocks into other data sources',
-      url='http://github.com/aaroncox/blocksync',
-      author='Aaron Cox',
-      author_email='aaron@greymass.com',
-      license='MIT',
-      packages=find_packages(),
-      zip_safe=False)
+setup(
+    name='blocksync',
+    version='0.1.0',
+    description='Utility to stream and sync blocks into other data sources',
+    url='http://github.com/aaroncox/blocksync',
+    author='Aaron Cox',
+    author_email='aaron@greymass.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['jsonrpcclient', 'requests'],
+    zip_safe=False
+)
