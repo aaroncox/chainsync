18287bed8a3a5ec12c76a88ac2bb4b637a903a33
Aaron Cox <aaron.cox@greymass.com>
2018-03-18 13:28:20 -0400

    added checks against available APIs to ensure they'll work

--- blocksync/adapters/steemv2/steem.py
+++ blocksync/adapters/steemv2/steem.py
@@ -56,29 +56,124 @@ class SteemV2Adapter(AbstractAdapter, BaseAdapter):
         opData['transaction_id'] = vop['trx_id']
         return opData
 
+    def is_api_available(self, api, method, raiseException=True):
+        if api not in self.apis:
+            if raiseException:
+                raise Exception('endpoint not capable of calling {}.{} ({} not available)'.format(api, method, api))
+            else:
+                return False
+        return True
+
     def get_block(self, block_num):
-        response = HttpClient(self.endpoint).request('block_api.get_block', block_num=block_num)
-        response['block']['block_num'] = int(str(response['block']['block_id'])[:8], base=16)
-        return response['block']
+        # block_api method
+        api = 'block_api'
+        method = 'get_block'
+        # ensure the API is available
+        if self.is_api_available(api, method):
+            response = HttpClient(self.endpoint).request('.'.join([api, method]), block_num=block_num)
+            response['block']['block_num'] = int(str(response['block']['block_id'])[:8], base=16)
+            return response['block']
 
     def get_ops_in_block(self, block_num, virtual_only=False):
-        return HttpClient(self.endpoint).request('condenser_api.get_ops_in_block', [block_num, virtual_only])
+        if self.is_api_available('account_history_api', 'get_ops_in_block', raiseException=False):
+            return self.get_ops_in_block_from_account_history_api(start_block=start_block, virtual_only=virtual_only, blocks=blocks)
+        elif self.is_api_available('condenser_api', 'get_ops_in_block', raiseException=False):
+            return self.get_ops_in_block_from_condenser_api(start_block=start_block, virtual_only=virtual_only, blocks=blocks)
+        else:
+            raise Exception('endpoint not capable of calling get_ops_in_block from either condenser_api or account_history_api')
+
+    def get_ops_in_block_from_account_history_api(self, block_num, virtual_only=False):
+        # account_history_api method
+        api = 'account_history_api'
+        method = 'get_ops_in_block'
+        response = HttpClient(self.endpoint).request('.'.join([api, method]), {
+            'block_num': block_num,
+            'only_virtual': virtual_only
+        })
+        return response['ops']
+
+    def get_ops_in_block_from_condenser_api(self, block_num, virtual_only=False):
+        # condenser_api method
+        api = 'condenser_api'
+        method = 'get_ops_in_block'
+        response = HttpClient(self.endpoint).request('.'.join([api, method]), [
+            block_num,
+            virtual_only
+        ])
+        return response
 
     def get_ops_in_blocks(self, start_block=1, virtual_only=False, blocks=10):
-        requests = [Request('condenser_api.get_ops_in_block', [i, virtual_only]) for i in range(start_block, start_block + blocks)]
+        if self.is_api_available('account_history_api', 'get_ops_in_block', raiseException=False):
+            return self.get_ops_in_blocks_from_account_history_api(start_block=start_block, virtual_only=virtual_only, blocks=blocks)
+        elif self.is_api_available('condenser_api', 'get_ops_in_block', raiseException=False):
+            return self.get_ops_in_blocks_from_condenser_api(start_block=start_block, virtual_only=virtual_only, blocks=blocks)
+        else:
+            raise Exception('endpoint not capable of calling get_ops_in_block from either condenser_api or account_history_api')
+
+    def get_ops_in_blocks_from_account_history_api(self, start_block=1, virtual_only=False, blocks=10):
+        # account_history_api method
+        api = 'account_history_api'
+        method = 'get_ops_in_block'
+        # assemble list with multiple requests for batch
+        requests = [
+            Request('.'.join([api, method]), {
+                'block_num': i,
+                'only_virtual': virtual_only
+            }) for i in range(start_block, start_block + blocks)
+        ]
+        # get response
         response = HttpClient(self.endpoint).send(requests)
+        # return the resulting ops
+        return [r['result']['ops'] for r in response]
+
+    def get_ops_in_blocks_from_condenser_api(self, start_block=1, virtual_only=False, blocks=10):
+        # condenser_api method
+        api = 'condenser_api'
+        method = 'get_ops_in_block'
+        # assemble list with multiple requests for batch
+        requests = [
+            Request('.'.join([api, method]), [
+                i,
+                virtual_only
+            ]) for i in range(start_block, start_block + blocks)
+        ]
+        # get response
+        response = HttpClient(self.endpoint).send(requests)
+        # return the resulting ops
         return [r['result'] for r in response]
 
     def get_blocks(self, start_block=1, blocks=10):
-        requests = [Request('block_api.get_block', block_num=i) for i in range(start_block, start_block + blocks)]
-        response = HttpClient(self.endpoint).send(requests)
-        return [dict(r['result']['block'], **{'block_num': int(str(r['result']['block']['block_id'])[:8], base=16)}) for r in response]
+        # block_api method
+        api = 'block_api'
+        method = 'get_block'
+        if self.is_api_available(api, method):
+            # assemble list with multiple requests for batch
+            requests = [
+                Request('.'.join([api, method]), {
+                    'block_num': i
+                }) for i in range(start_block, start_block + blocks)
+            ]
+            # get response
+            response = HttpClient(self.endpoint).send(requests)
+            # return the resulting block of each result
+            return [dict(r['result']['block'], **{'block_num': int(str(r['result']['block']['block_id'])[:8], base=16)}) for r in response]
 
     def get_config(self):
-        return HttpClient(self.endpoint).request('database_api.get_config')
+        # database_api method
+        api = 'database_api'
+        method = 'get_config'
+        if self.is_api_available(api, method):
+            return HttpClient(self.endpoint).request('.'.join([api, method]))
 
     def get_methods(self):
-        return HttpClient(self.endpoint).request('get_methods')
+        # jsonrpc method
+        api = 'jsonrpc'
+        method = 'get_methods'
+        return HttpClient(self.endpoint).request('.'.join([api, method]))
 
     def get_status(self):
-        return HttpClient(self.endpoint).request('database_api.get_dynamic_global_properties')
+        # database_api method
+        api = 'database_api'
+        method = 'get_dynamic_global_properties'
+        if self.is_api_available(api, method):
+            return HttpClient(self.endpoint).request('.'.join([api, method]))
