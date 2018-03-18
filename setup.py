9b81b97aa290f9b2cbc10a70d64b44c6e9dc2275
Aaron Cox <aaron.cox@greymass.com>
2018-03-18 14:34:26 -0400

    bumped to add dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='blocksync',
-      version='0.1.1',
+      version='0.1.2',
       description='Utility to stream and sync blocks into other data sources',
       url='http://github.com/aaroncox/blocksync',
       author='Aaron Cox',
       author_email='aaron@greymass.com',
       license='MIT',
       packages=find_packages(),
+      install_requires=[
+          'jsonrpcclient',
+      ],
       zip_safe=False)
