e98ef9657b9d4ea4609fe19e3bd2efd26cf79d1b
Aaron Cox <aaron.cox@greymass.com>
2018-03-18 20:32:46 -0400

    Optimized datetime generation from string

--- blocksync/adapters/steemv2/steem.py
+++ blocksync/adapters/steemv2/steem.py
@@ -29,13 +29,12 @@ class SteemV2Adapter(AbstractAdapter, BaseAdapter):
         ]
     }
 
+    def formatDate(self, ts):
+        return datetime(int(ts[:4]), int(ts[5:7]), int(ts[8:10]), int(ts[11:13]), int(ts[14:16]), int(ts[17:19]))
 
     def opData(self, block, opType, opData, txIndex=False):
         # Ensure the format of the timestamp as a datetime
-        if not isinstance(block['timestamp'], datetime):
-            opData['timestamp'] = datetime.strptime(block['timestamp'], '%Y-%m-%dT%H:%M:%S')
-        else:
-            opData['timestamp'] = block['timestamp']
+        opData['timestamp'] = self.formatDate(block['timestamp'])
         # Add some useful context to the operation
         opData['block_num'] = block['block_num']
         opData['operation_type'] = opType
@@ -46,10 +45,7 @@ class SteemV2Adapter(AbstractAdapter, BaseAdapter):
         # Extract the operation from the vop object
         opType, opData = vop['op']
         # Ensure the format of the timestamp as a datetime
-        if not isinstance(vop['timestamp'], datetime):
-            opData['timestamp'] = datetime.strptime(vop['timestamp'], '%Y-%m-%dT%H:%M:%S')
-        else:
-            opData['timestamp'] = vop['timestamp']
+        opData['timestamp'] = self.formatDate(vop['timestamp'])
         # Add some useful context to the operation
         opData['block_num'] = vop['block']
         opData['operation_type'] = opType
