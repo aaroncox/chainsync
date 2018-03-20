168c34931ce8a3fe7794648eb460fc75a01b1ecc
Aaron Cox <aaron.cox@greymass.com>
2018-03-19 22:58:33 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ChainSync',
-    version='0.1.4',
+    version='0.1.5',
     description='Utility to stream and sync blocks into other data sources',
     url='http://github.com/aaroncox/chainsync',
     author='Aaron Cox',
