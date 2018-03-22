8b9ee8076f048de059a0852816e770672e76f084
Aaron Cox <aaron.cox@greymass.com>
2018-03-22 01:32:42 -0400

    decent update

--- chainsync/adapters/decent/decent.py
+++ chainsync/adapters/decent/decent.py
@@ -16,7 +16,18 @@ class DecentAdapter(AbstractAdapter, BaseAdapter):
         # Add some useful context to the operation
         opData['block_num'] = block['block_num']
         opData['operation_type'] = opType
-        opData['timestamp'] = datetime.strptime(block['timestamp'], '%Y-%m-%dT%H:%M:%S')
+        opData['timestamp'] = block['timestamp']
+        # opData['transaction_id'] = block['transaction_ids'][txIndex]
+        return opData
+
+    def vOpData(self, vop):
+        # Extract the operation from the vop object format
+        opType, opData = vop
+        # Add some useful context to the operation
+        opData['block_num'] = opData['block']
+        opData['operation_type'] = opType
+        opData['timestamp'] = opData['timestamp']
+        # opData['transaction_id'] = vop['trx_id']
         return opData
 
     def get_block(self, block_num):
@@ -24,15 +35,27 @@ class DecentAdapter(AbstractAdapter, BaseAdapter):
         response['block_num'] = block_num
         return response
 
-    def get_blocks(self, start_block=1, blocks=10):
-        for i in range(start_block, start_block + blocks):
-            yield self.get_block(i)
+    def get_blocks(self, blocks):
+        for i in blocks:
+            yield self.call('get_block', block_num=int(i))
+
+    def get_ops_in_block(self, block_num, virtual_only=False):
+        block = self.call('get_block', block_num=block_num)
+        for tx in block['transactions']:
+            for op in tx['operations']:
+                op[1]['block'] = block_num
+                op[1]['timestamp'] = block['timestamp']
+                yield op
+
+    def get_ops_in_blocks(self, blocks, virtual_only=False):
+        for i in blocks:
+            yield self.call('get_ops_in_block', block_num=i, virtual_only=virtual_only)
 
     def get_config(self):
         return HttpClient(self.endpoint).request('get_config')
 
     def get_methods(self):
-        return []
+        return 'NOT_SUPPORTED'
 
     def get_status(self):
         return HttpClient(self.endpoint).request('get_dynamic_global_properties')
--- example_decent.py
+++ example_decent.py
@@ -5,23 +5,53 @@ from chainsync.adapters.decent import DecentAdapter
 adapter = DecentAdapter(endpoints=['http://api.decent-db.com:8090/'])
 chainsync = ChainSync(adapter)
 
-print('Getting block 1')
+print('\nGetting block 1')
 block = chainsync.get_block(1)
 print(block)
 
-print('Getting blocks 1-5')
-blocks = chainsync.get_blocks(1, 5)
+print('\nGetting blocks 1, 10, 50, 250, 500')
+blocks = chainsync.get_blocks([1, 10, 50, 250, 500])
 for block in blocks:
     print(block)
 
-print('Streaming blocks...')
-for block in chainsync.get_block_stream(batch_size=100, mode='irreversible'):
+print('\nGetting blocks 1000-1005')
+blocks = chainsync.get_block_sequence(1000, 5)
+for block in blocks:
+    print(block)
+
+print('\nGetting all ops in block 92847...')
+for op in chainsync.get_ops_in_block(92847):
+    print("{}: {} [{}] - {}".format(datetime.datetime.now(), op['block_num'], op['transaction_id'], op['operation_type']))
+
+print('\nGetting withdraw_vesting ops in block 92847...')
+for op in chainsync.get_ops_in_block(92847, whitelist=['withdraw_vesting']):
+    print("{}: {} [{}] - {}".format(datetime.datetime.now(), op['block_num'], op['transaction_id'], op['operation_type']))
+
+print('\nGetting all ops in block 10000, 50000, and 20000...')
+for op in chainsync.get_ops_in_blocks([10000, 50000, 20000]):
+	print("{}: {} [{}] - {}".format(datetime.datetime.now(), op['block_num'], op['transaction_id'], op['operation_type']))
+
+print('\nGetting producer_reward ops in block 10000, 50000, and 20000...')
+for op in chainsync.get_ops_in_blocks([10000, 50000, 20000], whitelist=['producer_reward']):
+	print("{}: {} [{}] - {}".format(datetime.datetime.now(), op['block_num'], op['transaction_id'], op['operation_type']))
+
+print('\nStreaming blocks, 100 at a time, from the irreversible height...')
+for dataType, block in chainsync.stream(['blocks'], batch_size=100, mode='irreversible'):
     print("{}: {} - {}".format(datetime.datetime.now(), block['block_num'], block['miner']))
 
-print('Streaming all ops...')
-for op in chainsync.get_op_stream():
-    print("{}: {} - {}".format(datetime.datetime.now(), op['block_num'], op['operation_type']))
+print('\nStreaming all ops...')
+for dataType, op in chainsync.stream(['ops']):
+    print("{}: {} [{}] - {}".format(datetime.datetime.now(), op['block_num'], op['transaction_id'], op['operation_type']))
 
-print('Streaming vote ops only...')
-for op in chainsync.get_op_stream(whitelist=['vote']):
-    print("{}: {} - {} by {}".format(datetime.datetime.now(), op['block_num'], op['operation_type'], op['voter']))
+print('\nStreaming all blocks + ops + virtual ops + accurate counts of ops per block...')
+for dataType, data in chainsync.stream(['blocks', 'ops', 'ops_per_block'], start_block=1045177):
+    dataHeader = "{} #{}: {}".format(datetime.datetime.now(), data['block_num'], dataType)
+    if dataType == "op":
+        print("{} {}".format(dataHeader, data['operation_type']))
+    if dataType == "block":
+        txCount = len(data['transactions'])
+        opCount = sum([len(tx['operations']) for tx in data['transactions']])
+        print("{} - #{} - tx: {} / ops: {}".format(dataHeader, data['block_num'], txCount, opCount))
+    if dataType == "ops_per_block":
+        for height in data:
+            print("{} - #{}: {}".format(dataHeader, height, data[height]))
