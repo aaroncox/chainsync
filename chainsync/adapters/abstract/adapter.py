754f45db8a1377870da05f16514788cfefd54c89
Aaron Cox <aaron.cox@greymass.com>
2018-04-06 02:26:13 -0400

    updated abstract with new format methods

--- chainsync/adapters/abstract/adapter.py
+++ chainsync/adapters/abstract/adapter.py
@@ -33,7 +33,7 @@ class AbstractAdapter(ABC):
     def get_blocks(self, blocks=[]):
         """ retrieve a block using the `get_block` method
 
-            :param int blocks: a list of the blocks to retrieve        
+            :param int blocks: a list of the blocks to retrieve
         """
         pass
 
@@ -54,32 +54,13 @@ class AbstractAdapter(ABC):
         pass
 
     @abstractmethod
-    def get_ops_in_transaction(self, transaction_id=1):
+    def format_op_from_get_block(self, block, op, txIndex=False, opIndex=False):
         pass
 
     @abstractmethod
-    def get_ops_in_transactions(self, transaction_ids=[]):
+    def format_op_from_get_ops_in_block(self, op):
         pass
 
     @abstractmethod
-    def opData(self, block, opType, opData, txIndex):
-        """ decorate the operation data with details from the block
-
-            :param dict block: the block this operation was contained within
-            :param string opType: the type of operation
-            :param dict opData: the operation as returned by the blockchain
-            :param int txIndex: the position of the operation in the transaction
-                    for lookup against the block data
-        """
-        pass
-
-    @abstractmethod
-    def vOpData(self, vop):
-        """ returns a formatted virtual op
-
-            decorate the virtual operation data to match the format of a
-            basic operation (most data exists, just arranged differently)
-
-            :return:
-        """
+    def format_op_from_get_transaction(self, tx, op, txIndex=False, opIndex=False):
         pass
